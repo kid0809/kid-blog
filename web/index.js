fb43c2f34a63a42ba28bb5e3a4aad3687a277d2f
kid0809 <182883415@qq.com>
2016-06-20 17:53:15 +0800

    前端环境

---
+++ web/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>111234567</div>
+    )
+  }
+}
+
+
+render(
+  <App />,
+  document.getElementById('root')
+)
