77cd73cf2d077b9b657f29323112228c4e09ebf4
kid <182883415@qq.com>
2017-02-09 11:04:04 +0800

    更新库，升级webpack1.x -> webpack2.2

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
     __dirname: true
   },
   entry: {
-    vendor: ['react', 'react-router', 'redux'],
-    css: ['./web/public/sass/main.scss'],
+    vendor: ['react', 'react-dom', 'react-router', 'redux'],
+    css: './web/public/sass/main.scss',
     components: [
       'webpack-hot-middleware/client',
       './web/index.js'
@@ -24,47 +24,60 @@ module.exports = {
     publicPath: '/bundle/',
     sourceMapFilename: 'map/[file].map'
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader?browsers=last 2 version'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', 'jsx']
+  },
+
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendor'),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("development")
       },
       'API_SERVER': JSON.stringify('http://localhost:8081')
     }),
+    new webpack.LoaderOptionsPlugin({
+      // test: /\.xxx$/, // may apply this only for some modules
+      minimize: false,
+      debug: true
+    })
   ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.scss/,
-        exclude: /node_modules/,
-        loader: 'style-loader!css-loader!postcss-loader!sass-loader?browsers=last 2 version'
-      },
-
-      {
-        test: /\.css/,
-        loader: 'style-loader!css-loader!postcss-loader'
-      },
-    ]
-  },
-  postcss: function () {
-      return [precss, autoprefixer];
-  },
-  resolve: {
-    extensions: ['', '.js']
-  }
 }
